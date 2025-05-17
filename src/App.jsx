be8c7ae24a55f7869b228bfe1dfb129de530ae3d
master <gubinvs@gmail.com>
2025-05-17 19:14:47 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,38 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode"; // Установите эту библиотеку: npm install jwt-decode
 import AuthorizationForm from "./RegistrationForm/AuthorizationForm.jsx";
 import Panel from "./Panel/Panel.jsx";
-import { isTokenValid } from "./isTokenValid.js";
+
+
+function isTokenValid(token) {
+  try {
+    const decoded = jwtDecode(token); // Декодируем токен
+    const currentTime = Math.floor(Date.now() / 1000); // Текущее время в секундах
+    return decoded.exp > currentTime; // Проверяем, не истек ли токен
+  } catch (error) {
+    console.error("Ошибка проверки токена:", error);
+    return false; // Если токен невалиден или не может быть декодирован
+  }
+}
+
+function ProtectedRoute({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  const token = localStorage.getItem("token");
+
+    if (token && isTokenValid(token)) {
+      setIsAuthenticated(true);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <div>Загрузка...</div>; 
+  }
+
+  return isAuthenticated ? children : <Navigate to="/Panel" replace />;
+}
 
 
 const App = () => {
@@ -14,10 +45,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<AuthorizationForm />} />
         <Route path="/Panel" element= {
-          <ProtectedRoute>
-            <Panel />
-          </ProtectedRoute>
-        } />
+                                        <ProtectedRoute>
+                                          <Panel />
+                                        </ProtectedRoute>
+                                      } />
       </Routes>
     </Router>
   );
--- src/Panel/Panel.jsx
+++ src/Panel/Panel.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import "./Panel.css";
+import { Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import "../index.css";
+import "./panel.css";
+
 
 
 const Panel = () => {
